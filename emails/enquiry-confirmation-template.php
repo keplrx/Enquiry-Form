2c439cb6d89caab617e535fd7b9a8173bb0b05cd
MH <minghaoooo06@gmail.com>
2025-06-23 14:12:02 +0800

    test1.3

--- emails/enquiry-confirmation-template.php
+++ emails/enquiry-confirmation-template.php
@@ -20,7 +20,6 @@
         </div>
         <p><strong>Hello <?php echo esc_html($form_data['name']); ?>!</strong></p>
         <p>Thank you for reaching out to us. We have received your enquiry, here are the details:</p>
-        <p>Thank you for reaching out to us. We have received your enquiry, here are the details:</p>
 
         <table class="table">
             <tr>
